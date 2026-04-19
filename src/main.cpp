9241087a900c1375d8f28bf9c4c228aeb42ab53d
Tommy McPhee <135555457+TommyMcPhee@users.noreply.github.com>
2026-04-19 04:43:52 +0000

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
 	ofGLFWWindowSettings settings;
 	settings.setGLVersion(3, 2); //we define the OpenGL version we want to use
 	settings.setSize(1024, 768);
+	settings.windowMode = OF_FULLSCREEN;
 	ofCreateWindow(settings);
-	ofSetFullscreen(false);
 	ofRunApp(new ofApp());
 }
